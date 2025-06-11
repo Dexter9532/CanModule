6a5e4e466ab1faa74f834bbdf8e02e24f206ee7f
bobo <back-engstrom@hotmail.com>
2025-06-11 12:39:53 +0200

    Finsh code working can module

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,16 @@
 
 const int SPI_CS_PIN = 9;           // Samma som i fungerande sändare
 const int FAN_PIN = A3;             // Fläktstyrning
+const int KILL_PIN = 8;
 mcp2515_can CAN(SPI_CS_PIN);        // Seed Studio CAN-klassen
 
 void setup() {
   Serial.begin(115200);
+
   pinMode(FAN_PIN, OUTPUT);
+  pinMode(KILL_PIN, INPUT);  // KILL-knapp som är aktiv låg
   digitalWrite(FAN_PIN, LOW);       // Fläkt av från start
+  
 
   // Initiera CAN med 100 kbps
   if (CAN.begin(CAN_100KBPS) == CAN_OK) {
@@ -47,19 +51,28 @@ void loop() {
     }
     Serial.println();
 
-    if (rxId == 0x459) {
-      if (buf[1] == 0x04) {
-        digitalWrite(FAN_PIN, HIGH);
-        Serial.println("Fläkt PÅ");
-        byte data[] = {0x01};
-        CAN.sendMsgBuf(0x460, 0, 1, data);
-      } 
-      else if (buf[1] == 0x00) {
-        digitalWrite(FAN_PIN, LOW);
-        Serial.println("Fläkt AV");
-        byte data[] = {0x00};
-        CAN.sendMsgBuf(0x460, 0, 1, data);
+    if (digitalRead(KILL_PIN) == LOW) {
+      if (rxId == 0x459) {
+
+        if (buf[1] == 0x04) {
+          digitalWrite(FAN_PIN, HIGH);
+          Serial.println("Fläkt PÅ");
+          byte data[] = {0x00}; // Skicka data för att slå på fläkten
+          CAN.sendMsgBuf(0x460, 0, 1, data);
+        } 
+        else if (buf[1] == 0x00) {
+          digitalWrite(FAN_PIN, LOW);
+          Serial.println("Fläkt AV");
+          byte data[] = {0x00}; // Skicka data för att slå på fläkten
+          CAN.sendMsgBuf(0x460, 0, 1, data);
+        }
       }
+    }
+    else {
+      digitalWrite(FAN_PIN, LOW); // Stäng av fläkten om KILL-knappen är nedtryckt
+      Serial.println("KILL-knapp nedtryckt, fläkt AV");
+      byte data[] = {0x01};
+      CAN.sendMsgBuf(0x460, 0, 1, data);
     }
   }
 }
